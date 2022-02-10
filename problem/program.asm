25b66959ef76100ffe311741cc54cd2e52b5826a
SEBAS <lariag94hd@gmail.com>
2022-02-09 19:09:34 -0500

    1

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,6 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// Sebastian Segovia Medina
+// 000371417
+// sebastian.segovia@upb.edu.co
 (START)
     @START
     0;JMP
