cebd254b756a8c05f22486d8d71a026bf17d9d15
SEBAS <lariag94hd@gmail.com>
2022-02-12 14:58:29 -0500

    4

--- problem/program.asm
+++ problem/program.asm
@@ -4,7 +4,7 @@
 
 //Variables
 
-(LEERTECLADO)
+(READKBD)
 @KBD //leer el teclado
 
 @70 //si la tecla "f" es presionada entonces pinte de negro los pixeles
@@ -20,7 +20,7 @@
 (BRAWBLACK) //pinta de negro los pixeles asignando un -1 de la RAM[16384]-RAM[24575]
     @0
     D=M
-    @FIN //si la RAM[0]=0 entonces salte al END
+    @END //si la RAM[0]=0 entonces salte al END
     D;JLE
     @2
     M=D
@@ -42,35 +42,37 @@
         MD=M-1
         @BLACKLOOP //salta a BLACKLOOP hasta que todas las direcciones tengan -1 de la RAM[16384]-RAM[24575] 
         D;JGT
-        @
+        @READKBD
+        0;JMP
 
-(DRAWWHITE)
+(DRAWWHITE) //pinta de negro los pixeles asignando un 0 de la RAM[16384]-RAM[24575]
     @1
     D=M
-    @FIN //si la RAM[1]=0 entonces salte al END
+    @END //si la RAM[1]=0 entonces salte al END
     D;JLE
-    @16
+    @5
     M=D
     @SCREEN
     D=A
-    @17
+    @6
     M=D
     (WHITELOOP)
-        @17
+        @6
         A=M
         M=-1 //pone -1 en la direccion de la memoria actual
-        @17
+        @6
         D=M
         @1
         D=D+A
-        @17
+        @6
         M=D
-        @16
+        @5
         MD=M-1
-        @BLACKLOOP //salta a BLACKLOOP hasta que todas las direcciones tengan -1 de la RAM[16384]-RAM[24575] 
+        @WHITELOOP //salta a BLACKLOOP hasta que todas las direcciones tengan -1 de la RAM[16384]-RAM[24575] 
         D;JGT
+        @READKBD
+        0;JMP
 
-
-(FIN)
-    @FIN
+(END)
+    @END
     0;JMP
