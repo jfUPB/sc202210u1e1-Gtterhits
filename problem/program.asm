8c6ea2e296f4e1dd1ea70380a20f20c002e801c7
SEBAS <lariag94hd@gmail.com>
2022-02-12 12:09:51 -0500

    2

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,25 @@
 // Sebastian Segovia Medina
 // 000371417
 // sebastian.segovia@upb.edu.co
-(START)
-    @START
+
+//Variables
+
+(LOOP)
+@KBD //Teclado
+
+@70 //Ref. tecla 'F'
+
+
+
+
+
+    @LOOP
+    0;JMP
+
+
+
+
+
+
+    @LOOP
     0;JMP
